c7f5062ee023ef3499266251f3e9ae92697019e5
benji1131 <benjamin.gremion@epfl.ch>
2021-10-22 12:21:19 +0200

    first commit

---
+++ plausbilité.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plausibilite = np.genfromtxt('plausibilite.csv', delimiter = ',')
+x = np.arange(0, plausibilite.shape[1] + 1) * 0.2
+y = 5 - np.arange(0, plausibilite.shape[0] + 1) * 0.2
+
+plt.axes().set_aspect('equal')
+plt.title('Plausibilité')
+plt.pcolormesh(x, y, plausibilite)
+plt.show()
