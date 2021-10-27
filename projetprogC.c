87613282b07712bb49dc6c53d6ee703d21433ab1
benji1131 <benjamin.gremion@epfl.ch>
2021-10-27 17:00:05 +0200

    aca

--- projetprogC.c
+++ projetprogC.c
@@ -1,5 +1,37 @@
-int main(int argc, char const *argv[]) {
-  printf("test");
-  printf("samere\n", );
-  return 0;
-}
+/// Projet : mesurer le niveau du lac en fct des précipitations.
+
+// étape 1 : calculer le débit IN en fonction des précipitation
+
+// étape 2 : Se renseigner sur le barage du seujet et connaitre le débit MAX OUT
+
+// étape 3 : Si débit out et plus grand que débit in -> estimer le niveau d'inondation
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+//-----------------------------------------------------------------------------------------------
+// STRUCTURE
+
+
+
+//-----------------------------------------------------------------------------------------------
+// FONCTIONS
+
+// soit trois type de personne: Saint S, Retablis R, Infecté I, (Dead D?), (Vacciné ?) -> somme = 1 (pourcentage)
+// durée ou on peut infecter les gens
+// virulence: 1 infecté peut contaminer cb de personne ?
+
+// taux de mortalité <3% (entre 2-3)
+
+// Vi
+
+//-----------------------------------------------------------------------------------------------
+//Fichiers
+
+
+
+
+//-----------------------------------------------------------------------------------------------
+//ETAPE1
